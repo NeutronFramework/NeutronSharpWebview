4b1294c31216336724137930b4a913c744a3286a
Annas <annaskherunas06@gmail.com>
2024-09-21 13:02:18 +0700

    Modernize the binding and add more method to manipulate the window only support windows and linux though cuz i have no mac

--- examples/DesktopApp/Program.cs
+++ examples/DesktopApp/Program.cs
@@ -1,46 +1,45 @@
 ﻿using System;
-using SharpWebview;
-using SharpWebview.Content;
+using SharpWebview.Scripts.Content;
+using SharpWebview.Scripts.Core;
 
-namespace DesktopApp
+namespace DesktopApp;
+
+class Program
 {
-    class Program
+    // Set the application to Single Threaded Apartment
+    // Otherwise the webview won't work on windows at least
+    [STAThread]
+    static void Main(string[] args)
     {
-        // Set the application to Single Threaded Apartment
-        // Otherwise the webview won't work on windows at least
-        [STAThread]
-        static void Main(string[] args)
-        {
-            // For a better startup expierience 
-            // I recommend to init the HostedContent in advance
-            var hostedContent = new HostedContent();
+        // For a better startup expierience 
+        // I recommend to init the HostedContent in advance
+        var hostedContent = new WebContent("app");
 
-            // Wrap the usage of the webview into a using block
-            // Otherwise the native window will not get disposed correctly.
-            // By setting the second parameter to true, sharpWebview intercepts
-            // all external links and opens them in the system browser.
-            using (var webview = new Webview(true, true))
-            {
-                webview
-                    // Set the title of the application window
-                    .SetTitle("The Lost Hitchhicker")
-                    // Set the start size of the window                
-                    .SetSize(1024, 768, WebviewHint.None)
-                    // Set the minimum size of the window
-                    .SetSize(800, 600, WebviewHint.Min)
-                    // Bind a c# function to the webview - Accessible with the name "evalTest"
-                    .Bind("evalTest", (id, req) =>
-                    {
-                        // Req contains the parameters of the javascript call
-                        Console.WriteLine(req);
-                        // And returns a successful promise result to the javascript function, which executed the 'evalTest'
-                        webview.Return(id, RPCResult.Success, "{ \"result\": 42 }");
-                    })
-                    // Navigate to this url on start
-                    .Navigate(hostedContent)
-                    // Run the webview loop
-                    .Run();
-            }
+        // Wrap the usage of the webview into a using block
+        // Otherwise the native window will not get disposed correctly.
+        // By setting the second parameter to true, sharpWebview intercepts
+        // all external links and opens them in the system browser.
+        using (var webview = new Webview(true, true))
+        {
+            webview
+                // Set the title of the application window
+                .SetTitle("The Lost Hitchhicker")
+                // Set the start size of the window                
+                .SetSize(400, 400)
+                .SetSize(400, 400, WebviewHint.Min)
+                .SetSize(600, 600, WebviewHint.Max)
+                // Bind a c# function to the webview - Accessible with the name "evalTest"
+                .Bind("evalTest", (id, req) =>
+                {
+                    // Req contains the parameters of the javascript call
+                    Console.WriteLine(req);
+                    // And returns a successful promise result to the javascript function, which executed the 'evalTest'
+                    webview.Return(id, RPCResult.Success, "{ \"result\": 42 }");
+                })
+                // Navigate to this url on start
+                .Navigate(hostedContent)
+                // Run the webview loop
+                .Run();
         }
     }
 }
